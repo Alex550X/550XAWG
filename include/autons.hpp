2aafd03732b029b1530b4b9401a9513cd225666e
Alex550X <151777306+Alex550X@users.noreply.github.com>
2023-11-22 13:13:21 -0500

    Add files via upload

---
+++ include/autons.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "EZ-Template/drive/drive.hpp"
+
+extern Drive chassis;
+
+void prog_skills();
+void far();
+void far2();
+void close();
+
+void default_constants();
+void one_mogo_constants();
+void two_mogo_constants();
